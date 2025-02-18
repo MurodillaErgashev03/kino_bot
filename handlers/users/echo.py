8240791e5cda10ffeeda32b29cb1531bbfb48385
Desktop <hikmatilloxon571@gmail.com>
2025-02-18 11:53:43 +0500

    fixed

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -3,9 +3,11 @@ from aiogram.fsm.context import FSMContext
 from keyboards.inline.buttons import generate_episode_buttons
 from loader import dp, db
 
+
 # Echo funksiyasi
 async def echo_message(message: types.Message):
     await message.answer(f"{message.text} - id bilan hech qanday kino yoki serial topilmadi ❌")
+
 
 # Kino yoki serialni qidirish
 @dp.message(F.text)
@@ -17,13 +19,17 @@ async def return_film_or_serial(message: types.Message, state: FSMContext):
 
     if film:
         kod = film.get('kod')  # Use get to avoid KeyError
-        if kod:  # Only proceed if 'kod' exists
+        file_id = film.get('file_id')  # Use get to safely access 'file_id'
+
+        if kod and file_id:  # Only proceed if 'kod' and 'file_id' exist
             text = (
                 f"⌨️ KOD: #{kod}\n"
                 f"{film['file_name']}\n\n"
                 f"📌 @Gold_kinolar_bot "
             )
-        await message.answer_video(film['file_id'], caption=text, parse_mode="HTML")
+            await message.answer_video(file_id, caption=text, parse_mode="HTML")
+        else:
+            await message.answer("Filmning video fayli mavjud emas yoki kodni topa olmadik.")
     else:
         # If no film found, search for the serial by name or ID
         serial = db.get_serial_by_name(user_input)
